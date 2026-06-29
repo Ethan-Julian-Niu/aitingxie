952096f91447419abff4850970508e90ed238996
Ethan-Julian-Niu <578675719@qq.com>
2026-06-29 09:00:37 +0800

    🎧 全新iDictation风格：多页架构/词书分类/精听训练/错词复习/统计

--- sw.js
+++ sw.js
@@ -1,39 +1,5 @@
-﻿const CACHE_NAME = 'dictation-v2';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'words.json',
-  'icon-192.png',
-  'icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(err => console.log('Cache addAll error:', err)))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request).then(response => {
-        if (response.status === 200) {
-          var clone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-        }
-        return response;
-      });
-    })
-  );
-});
+﻿const CACHE_NAME = 'idictation-v1';
+const ASSETS = ['index.html','manifest.json','words.json','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS).catch(()=>{})));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE_NAME).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(r.status===200){var rc=r.clone();caches.open(CACHE_NAME).then(ca=>ca.put(e.request,rc))}return r})))});
